faa137a48184532950093c7bfb13c0f2bd07c12d
Oskarp88 <oskyburgos81@gmail.com>
2023-02-22 19:11:45 -0500

    pedidos y productos

--- client/pages/productos.js
+++ client/pages/productos.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import Layout from '../components/Layout';
 
-const nosotros = () => {
+const Productos = () => {
     return ( 
         <div>
             <Layout>
-               <h2>Desde Nosotros</h2>
+              <h1 className='text-2xl text-gray-800 font-light'>Productos</h1>
             </Layout>
         </div>
      );
 }
  
-export default nosotros;
+export default Productos;
