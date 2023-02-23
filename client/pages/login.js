6badf99a692579e1c57772760310acda7e751a45
Oskarp88 <oskyburgos81@gmail.com>
2023-02-22 20:42:06 -0500

    formularios login y nuevacuenta

---
+++ client/pages/login.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import Layout from '../components/Layout';
+
+const Login = () => {
+    return ( 
+       <>
+         <Layout>
+           <h1 className='text-center text-2xl text-white font-light'>login</h1>
+            <div className='flex justify-center mt-5'>
+                <form className='bg-white rouded shadow-md px-8 pt-6 pb-8 mb-4'>
+                  <div className='mb-4'>
+                    <label className='blok text-gray text-sm font-bold mb-2' htmlForm="email">
+                       Email
+                    </label>
+
+                    <input 
+                       className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+                       id='email'
+                       type='email'
+                       placeholder='Email Usuario'
+                    />
+                  </div>
+                  <div className='mb-4'>
+                    <label className='blok text-gray text-sm font-bold mb-2' htmlForm="password">
+                       Password
+                    </label>
+
+                    <input 
+                       className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+                       id='password'
+                       type='password'
+                       placeholder='Password Usuario'
+                    />
+                  </div>
+                  <input 
+                     type='submit'
+                     className='bg-gray-800 w-full mt-5 p-2 text-white uppercase hover:bg-gray-900'
+                     value='Iniciar Sesión'
+                  />
+                </form>
+            </div>
+         </Layout>
+       </>
+     );
+}
+ 
+export default Login;
