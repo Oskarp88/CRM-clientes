1c81d209a9ffb073b711cc42d1d5acae613be788
Oskarp88 <oskyburgos81@gmail.com>
2023-02-22 19:00:19 -0500

    agregando estilos

--- client/components/Layout.js
+++ client/components/Layout.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import React from 'react'
+import Sidebar from './Sidebar';
 
 const Layout = ({children}) => {
     return ( 
@@ -9,7 +10,16 @@ const Layout = ({children}) => {
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
             <script src="https://cdn.tailwindcss.com"></script>
           </Head>
-          {children}
+          
+          <div className='bg-gray-200 min-h-screen'>
+            <div className='flex min-h-screen'> 
+                <Sidebar />
+                {children}
+            </div>
+           
+          </div>
+          
+          
         </>
      );
 }
