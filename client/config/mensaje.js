0fd7b603ead69809dffcba73b6f36ba800a785b6
Oscar Burgos <Oskyburgos81@gmail.com>
2023-03-06 08:31:28 -0500

    Delete client directory

--- client/config/mensaje.js
+++
@@ -1,9 +0,0 @@
-const mostrarMensaje = (mensaje) => {
-    return(
-       <div className='bg-white py-2 px-3 w-full my-3 max-w-sm text-center mx-auto'>
-          {mensaje}
-       </div>
-    )
- }
-
- export default mostrarMensaje;
